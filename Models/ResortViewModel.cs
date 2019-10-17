0122e35be8a3fd4d7c2635f90a3158a11b438046
lindseyshepard <55165590+lindseyshepard@users.noreply.github.com>
2019-10-17 13:55:48 -0400

    files
    updating

---
+++ Models/ResortViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Lindsey.Models
+{
+    public class ResortViewModel
+    {
+        public string State { get; set; }
+
+
+        public string ResortName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+        public List<SelectListItem> States { get; set; }
+
+        public ResortViewModel()
+        {
+            this.States = new List<SelectListItem>();
+
+        }
+
+
+
+    }
+}
